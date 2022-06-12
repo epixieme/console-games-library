0e39644ded536fcd771cc0a9838fa5611663aaff
epixieme <kirstie_hayes@hotmail.com>
2022-06-12 19:38:46 +0100

    updates view with list

--- public/main.js
+++ public/main.js
@@ -1,9 +1,18 @@
-// const { async } = require("jshint/src/prod-params")
+const { async } = require("jshint/src/prod-params")
 
-// const submit = document.querySelector('.submit')
-// async function addGame(){
+const edit= document.querySelector('.editGame')
+const update = document.querySelector('.update')// add this to ejs and do a hide and show when edit button is clicked
+update.addEventListener('click',  editGame)
 
-//     await fetch()
+  async function editGame (){
 
-// }
+      await fetch('/names', {
+        method: 'put',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          title: `${title.value}`,// have to find a way to update the field on input
+          release: `${title.release}`
+        })
+      })
 
+  }
--- server.js
+++ server.js
@@ -2,8 +2,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const MongoClient = require("mongodb").MongoClient;
 const app = express();
+
 require("dotenv").config();
+
+
 // console.log(process.env);
+
+// enables express to read info from forms
 
 MongoClient.connect(process.env.MONGO_URI, {
   useUnifiedTopology: true,
@@ -11,20 +16,39 @@ MongoClient.connect(process.env.MONGO_URI, {
   .then((client) => {
     console.log("Connected to Database");
     const db = client.db("console-games-library"); // renames the db
-    const quotesCollection = db.collection("games"); //creates the collection
+    const gamesCollection = db.collection("games"); //creates the collection
 
-    // All your handlers here...
 
-    app.get("/", (req, res) => {
-      res.sendFile(__dirname + "/index.html");
-    });
-    app.post("/games", (req, res) => {
-        quotesCollection.insertOne(req.body)
+
+  // ========================
+    // Middlewares
+    // ========================
+    app.set('view engine', 'ejs')
+    // Make sure you place body-parser before your CRUD handlers!
+    app.use(bodyParser.urlencoded({ extended: true }))
+    app.use(bodyParser.json())
+    app.use(express.static('public'))// make public folder accessible
+// ========================
+    // Routes
+    // ========================
+        // All your handlers here...
+    app.get('/', (req, res) => {
+        db.collection('games').find().toArray()
+          .then(data => {
+            res.render('index.ejs', { games: data })
+          })
+          .catch(error=>console.log(error))
+      })
+
+    app.post("/games", (req, res) => {// takes in the action route from the form
+       gamesCollection.insertOne(req.body)//inserts into db
+        // why did leon add individuls eg req.body.title
         .then(result => {
           console.log(result)
         })
         .catch(error => console.error(error))
     });
+    app.put()
 
   })
   .catch((error) => console.error(error));
@@ -34,6 +58,4 @@ app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
-// Make sure you place body-parser before your CRUD handlers!
-// enables express to read info from forms
-app.use(bodyParser.urlencoded({ extended: true }));
+
