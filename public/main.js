be8ad4f12bbb085151898a474fbcd62e0665d01a
epixieme <kirstie_hayes@hotmail.com>
2022-06-19 19:18:53 +0100

    solution

--- public/main.js
+++ public/main.js
@@ -19,35 +19,34 @@ const deleteArray = Array.from(deleteGame).forEach((element) => {
 function editGameInfo(event) {
   const target = event.target;
   let fields = target.parentNode.parentNode.children;
-  console.log( target.parentNode.parentNode.childNodes)
+  console.log(target.parentNode.parentNode.childNodes);
   let fieldsArray = Array.from(fields);
-  console.log('this is fieldsarray' + fieldsArray)
+  console.log("this is fieldsarray" + fieldsArray);
   //update childnodes to reflect additon of cards section etc
 
   console.log(document.getElementsByName("title")[0].value); /// how can I make this section dryer - maybe an object? could I use this query selector and a conidtion? https://bobbyhadz.com/blog/javascript-get-data-attribute-from-event-object
   // grabs all of the dom element (span) innertext for the target
-  const id = target.parentNode.parentNode.childNodes[1].innerText;/// take it from the live value?
-  console.log(id)
+  const id = target.parentNode.parentNode.childNodes[1].innerText; /// take it from the live value?
+  console.log(id);
   const title = target.parentNode.parentNode.childNodes[5].innerText;
-  console.log(title)
+  console.log(title);
   const release = target.parentNode.parentNode.childNodes[9].innerText;
   const developer = target.parentNode.parentNode.childNodes[13].innerText;
   const platform = target.parentNode.parentNode.childNodes[17].innerText;
   // console.log(title);
 
   if (target.classList.contains("editGame")) {
-    fieldsArray.forEach(item=>{
+    fieldsArray.forEach((item) => {
       item.setAttribute("contenteditable", true);
-      item.style.border='1px solid grey';
-      item.style.borderRadius='10px'
-      
-    })
+      item.style.border = "1px solid grey";
+      item.style.borderRadius = "10px";
+    });
     // document.querySelectorAll('.elements').contenteditable.foreach(item=>item.style.border='red solid 1px')
   } else if (target.classList.contains("updateGame")) {
-    fieldsArray.forEach(item=>{
-  item.setAttribute("contenteditable", false)
-  item.style.border='none';
-      item.style.borderRadius='10px'
+    fieldsArray.forEach((item) => {
+      item.setAttribute("contenteditable", false);
+      item.style.border = "none";
+      item.style.borderRadius = "10px";
     });
     // get the element text from the fields and put into post below on update
     updateGameInfo(id, title, release, developer, platform);
@@ -73,12 +72,11 @@ async function deleteGameInfo(event) {
   console.log("delete");
   const target = event.target;
 
-
   // console.log(document.querySelectorAll(".elements")); /// how can I make this section dryer - maybe an object? could I use this query selector and a conidtion? https://bobbyhadz.com/blog/javascript-get-data-attribute-from-event-object
   // grabs all of the dom element (span) innertext for the target
 
   const title = target.parentNode.parentNode.childNodes[5].innerText;
- 
+
   try {
     const response = await fetch("/deleteGames", {
       method: "delete",
@@ -92,3 +90,5 @@ async function deleteGameInfo(event) {
     location.reload();
   } catch (err) {}
 }
+
+
